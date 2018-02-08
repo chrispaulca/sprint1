4fe1a5c601132943a6eb5d228867f75fd5315fac
siavash mortezavi <siavashmortezavi@siavashs-MacBook-Pro.local>
2018-02-08 15:30:44 -0800

    deploy added

---
+++ deploy.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import paramiko
+import time
+
+def deploy(path_to_ssh_key_private_key, server_address, prefix):
+    print "Connecting to box"
+    print ""
+    k = paramiko.RSAKey.from_private_key_file(path_to_ssh_key_private_key)
+    c = paramiko.SSHClient()
+    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print "connecting"
+    c.connect(hostname = server_address, username = "ubuntu", pkey = k ) # change back username 
+    print "connected"
+
+    c.exec_command("rm -rf sprint1; git clone https://github.com/chrispaulca/sprint1.git")
+    print "Pull from Sprint1 successful"
+    time.sleep(10)
+    print "Script fully executed ... exiting"
+    c.exec_commant("cron 
+    c.close()
+deploy('/Users/siavashmortezavi/data/comp/deeplearningkey/smortezavi.pem','ec2-54-218-42-161.us-west-2.compute.amazonaws.com','anchor')
+
