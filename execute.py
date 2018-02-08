787a3ae982bbf0bf4e578bf620b72beaa44fe426
Ernest Kim <ekim22@dons.usfca.edu>
2018-02-08 15:56:35 -0800

    draft execute.py

--- execute.py
+++ execute.py
@@ -2,11 +2,8 @@ import os
 import json
 import sys
 
-#NOTE 1 : MAKE THIS TAKE AN ARGUMENT FROM SYS
-#NOTE 2 : WRITE TO FILE NAMED : prefix.txt > where prefix should be the variable passed from NOTE1
+prefix = sys.argv[1]
 
-#NOTE 1
-prefix = 'anchor'
 def extract_keys(d):
     keys = []
     for k,v in d.items():
@@ -18,12 +15,15 @@ def no_dupe_keys(d):
     keys_list = extract_keys(d)
     print('stuff')
 
-    ct = 0
+    ct_a = 0
+    ct_n = 0
     for i in keys_list:
-        if i == 'age' or i == 'name':
-            ct += 1
-    
-    if ct == 2:
+        if i == 'age':
+            ct_a += 1
+        if i == 'name':
+            ct_n += 1
+
+    if ct_a == 1 and ct_n == 1:
         return(True)
     else:
         return(False)
@@ -34,6 +34,10 @@ def extract_json(lines):
         try:
             blob = json.loads(line)
             name, age = blob['name'], blob['prop']['age']
+            try:
+                int(age)
+            except
+                continue
             if name != "" and age != "" and no_dupe_keys(blob):
                 outputs = outputs + [str(name) + ' ' + str(age)]
         except Exception as e:
@@ -43,24 +47,22 @@ def extract_json(lines):
 
 outputs = []
 
-def execute(prefix)
-    try:
-        os.chdir('./srv/runme')
+try:
+    os.chdir('/srv/runme')
 
-        print('Directory changed.')
-        for file in os.listdir('.'):
-            if file.startswith(prefix):
-                try:
-                    with open(file) as txt_file:
+    print('Directory changed.')
+    for file in os.listdir('.'):
+        if file.startswith(prefix):
+            try:
+                with open(file) as txt_file:
                         openned_file = txt_file.readlines()
                         outputs = outputs + extract_json(openned_file)
-                        # NOTE2
-                except:
-                    print file, ': error in JSON'
-    except:
-        print 'Sorry, this directoy does not exist.'
+            except:
+                print file, ': error in JSON'
+except:
+    print 'Sorry, this directoy does not exist.'
 
-    to_print = '\n'.join(outputs)
+to_print = '\n'.join(outputs)
 
-    with open(prefix+'.txt',w) as f:
-        write(to_print)
+with open('/srv/runme/'+prefix+'.txt',w) as f:
+    write(to_print)
